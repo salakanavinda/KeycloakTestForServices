acf40f1569e24a66a1ac37b84a30eb79e3970ee0
Salaka Navinda <mmsnavinda@gmail.com>
2020-08-13 15:13:18 +0530

    Initiated

---
+++ code/micro-service-apiOne/src/main/java/com/navinda/microservice/microserviceapiOne/MicroServiceApiOneApplication.java
@@ -0,0 +1,13 @@
+package com.navinda.microservice.microserviceapiOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApiOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceApiOneApplication.class, args);
+	}
+
+}
---
+++ code/micro-service-apiOne/src/test/java/com/navinda/microservice/microserviceapiOne/MicroServiceApiOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navinda.microservice.microserviceapiOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceApiOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
